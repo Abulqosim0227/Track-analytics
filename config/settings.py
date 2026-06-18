5c701672972ea41c879ddd64fa8aeec32848a6e2
Nihol2025 <a.rafikov@nihol.uz>
2026-06-18 12:34:18 +0500

    Set LOGIN_REDIRECT_URL to avoid /accounts/profile 404

--- config/settings.py
+++ config/settings.py
@@ -72,6 +72,9 @@ AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
 ]
 
+LOGIN_REDIRECT_URL = "/admin/"
+LOGOUT_REDIRECT_URL = "/admin/login/"
+
 LANGUAGE_CODE = "en-us"
 TIME_ZONE = "Asia/Tashkent"
 USE_I18N = True
